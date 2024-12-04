ebbd69ade93c47352fae43e75f09a8b2a6c1c510
TarekSalha288 <salhtt829@gmail.com>
2024-12-04 21:32:11 +0300

    Update ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,14 +28,32 @@ if($fav){
 return response()->json(['message'=>'You Can\'t Delete This '],400);
     }
     public function showProducts($category)
-    {
-        $products = Product::where('category', $category)->simplePaginate(10);
+    {$products = Product::where('category',$category)->paginate(10);
+        $allProducts = [];
         if ($products->isEmpty()) {
-            return response()->json(['message' => 'No Products For This Category'], 400);
+            return response()->json(['message' => 'No Products Available'], 400);
         }
-        return response()->json([
-            'data' => $products,
-        ], 200);
+        foreach ($products as $product) {
+            $fav = DB::table('favorite')
+                ->where('user_id', auth()->user()->id)
+                ->where('product_id', $product->id)
+                ->exists();
+
+            $owner = Store::where('id', $product->store_id)->first();
+$product->toArray();
+$product['owner']=$owner->store_name;
+$product['fav']=$fav;
+            $allProducts[] = [
+                'product' => $product,
+            ];
+        }
+     return response()->json([ 'data' => $allProducts,
+     'pagination' => [ 'current_page' => $products->currentPage(),
+     'last_page' => $products->lastPage(),
+     'total' => $products->total(),
+     'per_page' => $products->perPage(),
+     'next_page_url' => $products->nextPageUrl(),
+     'prev_page_url' => $products->previousPageUrl(), ], ], 200);
     }
 
     public function allProducts()
